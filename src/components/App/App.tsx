566b347bd1dfa7927aef51084f09e18e111cc5d4
Alina <alinavinnikhgfff@gmail.com>
2026-05-20 11:47:39 +0300

    add pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,26 +5,42 @@ import { fetchNotes } from "../../services/noteService";
 import { useState } from "react";
 import SearchBox from "../SearchBox/SearchBox";
 import { useDebouncedCallback } from "use-debounce";
+import Pagination from "../Pagination/Pagination";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [page, setPage] = useState(1);
+  // const [noteId, setNoteId] = useState("");
 
-  const { data } = useQuery({
-    queryKey: ["getNotes", searchQuery],
+  const { data, isSuccess } = useQuery({
+    queryKey: ["notes", searchQuery, page],
     queryFn: () => fetchNotes(searchQuery, page),
     placeholderData: keepPreviousData,
   });
+
+  // const mutation = useMutation({
+  //   mutationFn: (noteId: string) => deleteNote(noteId),
+  // });
 
   const onClickDelete = () => {};
   const onChange = useDebouncedCallback((newSearchValue: string) => {
     setSearchQuery(newSearchValue);
   }, 300);
+
+  const totalPages = data?.totalPages ?? 0;
+
+  //Markup
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
         <SearchBox onChange={onChange} />
-        {/* Пагінація */}
+        {isSuccess && totalPages > 1 && (
+          <Pagination
+            forcePage={page - 1}
+            pageCount={totalPages}
+            onPageChange={setPage}
+          />
+        )}
         {/* Кнопка створення нотатки */}
       </header>
       {data && data.notes.length > 0 && (
